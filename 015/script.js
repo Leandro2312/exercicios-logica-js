d00e5133e07e0e54e57d20637380634d6eb950b0
Leandro2312 <94083259+Leandro2312@users.noreply.github.com>
2022-02-10 22:07:05 -0300

    Resolução dos exercícios

---
+++ 015/script.js
@@ -0,0 +1,10 @@
+//Estrutura de decisão
+let idade
+console.log("Informe sua idade")
+// depois teremos a interação e entrada de dados
+idade = 18
+if(idade >= 18) {
+    console.log("Você é maior de idade")
+} else {
+    console.log("Você não pode dirigir")
+}
